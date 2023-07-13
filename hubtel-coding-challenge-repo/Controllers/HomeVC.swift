b04d68885f4cf04f1e68b2d960e72e9b4839eb30
kwame_boateng <kwameagyenimboateng64@gmail.com>
2023-07-13 18:03:08 +0000

    etc

--- hubtel-coding-challenge-repo/Controllers/HomeVC.swift
+++ hubtel-coding-challenge-repo/Controllers/HomeVC.swift
@@ -14,6 +14,7 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         setupViews()
         setupContraints()
         
